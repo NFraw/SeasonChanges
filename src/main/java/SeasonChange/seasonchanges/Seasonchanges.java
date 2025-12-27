9bc210295e13e8f589db6fe8d7112d282dac798a
Z Nan <149232318+NFraw@users.noreply.github.com>
2025-12-27 12:40:49 +0800

    统一管理前提交本地修改

--- src/main/java/SeasonChange/seasonchanges/Seasonchanges.java
+++ src/main/java/SeasonChange/seasonchanges/Seasonchanges.java
@@ -1,10 +1,18 @@
 package SeasonChange.seasonchanges;
 
-import net.fabricmc.api.ModInitializer;
+import SeasonChange.seasonchanges.items.ModItems;import net.fabricmc.api.ModInitializer;import org.slf4j.LoggerFactory;import java.util.logging.Logger;
 
 public class Seasonchanges implements ModInitializer {
 
+    public static final String MOD_ID = "seasonchanges";
+
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+
+        ModItems.initialize();
+//        初始化items
+        LOGGER.info("Hello Fabirc World!");
     }
 }
--- src/main/java/SeasonChange/seasonchanges/items/ModItems.java
+++ src/main/java/SeasonChange/seasonchanges/items/ModItems.java
@@ -1,4 +1,25 @@
 package SeasonChange.seasonchanges.items;
 
-public class ModItems {
+import SeasonChange.seasonchanges.Seasonchanges;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public final class ModItems {
+
+    private ModItems() {}
+
+    // 新物品的实例
+    public static final Item ICE_CREAM = register("ice_cream", new Item(new Item.Settings()));
+
+    public static <T extends Item> T register(String id, T item) {
+        // 对于 1.21 之前的版本，请将 ''Identifier.of'' 替换为 ''new Identifier''
+        return Registry.register(Registries.ITEM, Identifier.of(Seasonchanges.MOD_ID, id), item);
+    }
+
+    public static void initialize() {
+        Seasonchanges.LOGGER.info("Resgistering Items");
+    }
+
 }
